39d164acff1e587dfe38315df2212c2c7c75a9db
Pratyush-Chouksey <pratyushchouksey1983@gmail.com>
2025-03-27 18:38:14 +0530

    Done With 6 modules

--- src/6 Tuples/2 Enums.ts
+++ src/6 Tuples/2 Enums.ts
@@ -1,1 +1,19 @@
 // In TypeScript, an enum is a way to define a set of named constants. Enums allow you to define a collection of related values that can be used interchangeably in your code.
+
+enum WeatherConditions {
+    Sunny,
+    Cloudy,
+    Rainy,
+    Snowy,
+  }
+
+console.log(WeatherConditions.Sunny) // This will o/p:- the index of sunny i.e., 0
+
+enum WeatherConditions1 {
+    Sunny = "sunny",
+    Cloudy = "Cloudy",
+    Rainy = "Rainy",
+    Snowy = "Snowy",
+  }
+const currentWeather = WeatherConditions1.Sunny
+console.log(currentWeather) // This will give sunny as an output
